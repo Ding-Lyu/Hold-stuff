b7848b5b72a997288f147f7753f107388cf2f84b
Ding Lyu <lyudingguangnz@outlook.com>
2022-05-10 00:25:45 +1200

    Ex 2 and 3 done

---
+++ jsEx/ex3.js
@@ -0,0 +1,11 @@
+function stars(n) {
+  let result = "";
+  for (let i = 0; i < n; i++) {
+    result += "*";
+  }
+  return result;
+}
+
+console.log(stars(5));
+console.log(stars(3));
+console.log(stars(10));
