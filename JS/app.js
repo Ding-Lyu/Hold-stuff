6a40026685ffc5d39775ddd5de90d3e608dedeeb
Ding Lyu <lyudingguangnz@outlook.com>
2022-05-13 22:43:08 +1200

    JS Ex

--- JS/app.js
+++ JS/app.js
@@ -1,24 +1,27 @@
-let answer = Math.floor(Math.random() * 100);
-console.log(answer);
+// let answer = Math.floor(Math.random() * 100);
+// console.log(answer);
 
-let n1 = 0;
-let n2 = 99;
+// let n1 = 0;
+// let n2 = 99;
 
-while (true) {
-  let guess = prompt("Make your guess: " + n1 + " and " + n2);
+// while (true) {
+//   let guess = prompt("Make your guess: " + n1 + " and " + n2);
 
-  if (guess < n1 || guess > n2) {
-    alert("Please input a valid number.");
-    continue;
-  }
+//   if (guess < n1 || guess > n2) {
+//     alert("Please input a valid number.");
+//     continue;
+//   }
 
-  if (guess == answer) {
-    alert("The secret number is: " + answer + ".");
-    alert("You win the game!");
-    break;
-  } else if (guess <= answer) {
-    n1 = guess;
-  } else if (guess >= answer) {
-    n2 = guess;
-  }
-}
+//   if (guess == answer) {
+//     alert("The secret number is: " + answer + ".");
+//     alert("You win the game!");
+//     break;
+//   } else if (guess <= answer) {
+//     n1 = guess;
+//   } else if (guess >= answer) {
+//     n2 = guess;
+//   }
+// }
+let secondH1 = document.querySelectorAll(".second");
+
+console.log(secondH1);
--- JS/getTry1.js
+++
@@ -1,5 +0,0 @@
-let try1 = require("./try1");
-
-let myName = "Ding";
-
-try1.night(myName);
